2332b2f767cbfe9b8cc4bcff0a3bb9da64ed2aa9
Anni-F <57504984+Anni-F@users.noreply.github.com>
2019-11-07 22:08:26 +0100

    Initial commit

---
+++ Assets/Scripts/FollowPlayer_03.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer_03 : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public Vector3 offset = new Vector3(-121, 6, 155);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vektor3 hier, weil die Kamera nicht direkt Pos. des Spielers, soll Abstand zu Auto. Kamera vorher auf 0, 0, 0 setzen
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController_03.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_03 : MonoBehaviour
+{
+    //define variables(steuerung), variables consist of;
+    //access modifier     data type   variable name    variable value
+    //NOCH FESTLEGEN und einen private float machen
+    public float speed = 25.5f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {   //get input    siehe Dokumentation input.GetAxis, von dort einfach die Tastaturbelegung übernehmen, die horizontalen für r/l
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //turn vehicle sideways based on player input up/down
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //move vehicle forward, when arrow keys right and/or left 
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
